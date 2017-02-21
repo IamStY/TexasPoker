00378d3cf1a6fb51e663ad05745df86ec79ec0e5
Yang_Cheng_Kuang <steven@brave.bot>
2017-02-21 10:46:50 +0800

    deal cards

---
+++ MyApplication/app/src/main/java/com/example/stevenyang/texaspokerproject/Cards.java
@@ -0,0 +1,26 @@
+package com.example.stevenyang.texaspokerproject;
+
+/**
+ * Created by User on 2017/2/21.
+ */
+
+public class Cards {
+    int color; //0 : 梅花 1:方塊 2:愛心 3:黑桃
+    int number;
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
--- MyApplication/app/src/main/java/com/example/stevenyang/texaspokerproject/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/stevenyang/texaspokerproject/MainActivity.java
@@ -2,12 +2,70 @@ package com.example.stevenyang.texaspokerproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinkedList<Cards> player1 = new LinkedList();
+    LinkedList<Cards> player2 = new LinkedList();
+    LinkedList<Cards> deck = new LinkedList();
+    ArrayList<Cards> disposedCards = new ArrayList();
+    Random ran = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player1 = giveCardsToPlayer(2);
+        player2 = giveCardsToPlayer(2);
+        deck = giveCardsToPlayer(5);
+        for (int i=0;i<player1.size();i++) {
+            Log.i("player 1 cards" ,"color:"+player1.get(i).getColor()+" num:"+player1.get(i).getNumber());
+        }
+        for (int i=0;i<player2.size();i++) {
+            Log.i("player 2 cards" ,"color:"+player2.get(i).getColor()+" num:"+player2.get(i).getNumber());
+        }
+        for (int i=0;i<deck.size();i++) {
+            Log.i("deck cards" ,"color:"+deck.get(i).getColor()+" num:"+deck.get(i).getNumber());
+        }
+        for (int i=0;i<disposedCards.size();i++) {
+            Log.i("disposedCards cards" ,"color:"+disposedCards.get(i).getColor()+" num:"+disposedCards.get(i).getNumber());
+        }
     }
+
+    private LinkedList<Cards> giveCardsToPlayer(int amount) {
+
+        LinkedList<Cards> tempCards = new LinkedList<Cards>();
+
+        while (tempCards.size()<amount){
+           Cards card = createCards();
+            boolean giveCard = true;
+            for (int i=0 ; i<disposedCards.size();i++) {
+                if(disposedCards.get(i).getColor()==card.getColor()&&disposedCards.get(i).getNumber()==card.getNumber()){
+                    giveCard = false;
+                    break;
+                }
+
+            }
+            if(giveCard){
+                tempCards.add(card);
+                disposedCards.add(card);
+            }
+
+        }
+        return    tempCards;
+
+    }
+    private Cards createCards(){
+        int randNum =ran.nextInt(13)+1;
+        int randColor = ran.nextInt(3);
+        Cards cards = new Cards();
+        cards.setColor(randColor);
+        cards.setNumber(randNum);
+        return cards;
+    }
+
 }
+
